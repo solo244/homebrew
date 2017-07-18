b115c33f752483352dcc6524254797e50bc13200
ken <ken@yappa.be>
2017-07-18 07:45:42 +0200

    Added weapons and armor

---
+++ dev/js/main/search_feats.js
@@ -0,0 +1,35 @@
+(function ($) {
+
+  /*
+   * Vars
+   */
+  var $body = $("html, body");
+  $tut_search.on("keyup", getResults);
+  $tut_empty.on("click", emptySearch);
+
+  /*
+   *
+   */
+  function getResults(e){
+    var $this = $(this).val().toLowerCase();
+
+    if($this === ""){
+      $all_tutorials_blocks.removeClass("show_result");
+    }
+    else{
+      $all_tutorials_blocks.each(function(){
+        var content = $(this).find("h3").text().toLowerCase();
+        $(this).removeClass("show_result");
+        if(content.indexOf($this) >= 0){
+          $(this).addClass("show_result");
+        }
+      });
+    }
+  }
+
+  function emptySearch(){
+    $tut_search.val("");
+    $all_tutorials_blocks.removeClass("show_result");
+  }
+
+})(jQuery);
