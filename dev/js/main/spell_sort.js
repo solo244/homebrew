2e5ce8f4b297705fbe6a782601b62f77e4a4dc03
ken <ken@yappa.be>
2017-07-25 21:07:58 +0200

    Added animation to sorting and step3

--- dev/js/main/spell_sort.js
+++ dev/js/main/spell_sort.js
@@ -30,8 +30,6 @@
       var $this = $(this),
           $text = $this.closest("li").text().trim();
 
-      console.log(selected_c1);
-
       if($this.is(":checked")) {
         if($text === "Cantrip") {
           $text = "0";
@@ -40,11 +38,49 @@
         }
         selected_c1.push($text);
       }
-      if(selected_c1.length === 0) {
-        selected_c1 = ["0", "1", "2", "3", "4", "5", "6"];
+    });
+
+    $sort_c2.find("input").each(function() {
+      var $this = $(this),
+          $text = $this.closest("li").text().trim();
+
+      if($this.is(":checked")) {
+        selected_c2.push($text);
       }
     });
 
+    $sort_c3.find("input").each(function() {
+      var $this = $(this),
+          $text = $this.closest("li").text().trim();
+
+      if($this.is(":checked")) {
+        if($text === "No") {
+          $text = "-";
+        }
+        selected_c3.push($text);
+      }
+    });
+
+    if(selected_c1.length === 0) {
+      selected_c1 = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+    }
+    if(selected_c2.length === 0) {
+      selected_c2 = ["Abjuration", "Conjuration", "Divination", "Enchantment", "Evocation", "Illusion", "Necromancy", "Transmutation"];
+    }
+    if(selected_c3.length === 0) {
+      selected_c3 = ["Yes", "No"];
+    }
+    if(selected_c4.length === 0) {
+      selected_c4 = ["0", "1", "2", "3", "4", "5", "6", "7"];
+    }
+    if(selected_c5.length === 0) {
+      selected_c5 = ["0", "1", "2", "3"];
+    }
+    if(selected_c6.length === 0) {
+      selected_c6 = ["0", "1", "2"];
+    }
+
+    // Show/hide spells
     $spells.find(".spell").each(function() {
       var $this = $(this),
           $level = $this.find(".c2").text(),
@@ -54,13 +90,27 @@
           $components = $this.find(".c6").text(),
           $concentration = $this.find(".c7").text();
 
-      var spellsMatch = selected_c1.includes($level);
-      if(spellsMatch) {
-        $this.show();
+      var spellsMatch1 = selected_c1.includes($level),
+          spellsMatch2 = selected_c2.includes($school);
+          spellsMatch3 = selected_c3.includes($ritual);
+
+      if(spellsMatch1 && spellsMatch2 && spellsMatch3) {
+        $this.removeClass("hide-spell");
       } else {
-        $this.hide();
+        $this.addClass("hide-spell");
+        $this.removeClass("active");
       }
     });
+
+    // Animate to top
+    var timer = setInterval(function(){ delayAnimation() }, 100);
+
+    function delayAnimation() {
+      $('html, body').animate({
+          scrollTop: $sorting.offset().top
+      }, 800);
+      clearInterval(timer);
+    }
   }
 
 })(jQuery);
