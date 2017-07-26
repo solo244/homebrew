071c7e3f16bbd4a5cbb2365f1f5b80c9c8aa60dc
ken <ken@yappa.be>
2017-07-26 07:58:52 +0200

    Added proper structure for melee weapons

--- dev/js/main/search_feats.js
+++ dev/js/main/search_feats.js
@@ -7,7 +7,7 @@
       $search = $body.find(".search-block"),
       $search_field = $search.find(".search-field input"),
       $search_empty = $search.find(".search-field .clear"),
-      $result_blocks = $body.find(".search-result .feat");
+      $result_blocks = $body.find(".search-result .seRe");
 
   $search_field.on("keyup", getResults);
   $search_empty.on("click", emptySearch);
@@ -17,13 +17,14 @@
    */
   function getResults(e){
     var $this = $(this).val().toLowerCase();
+    console.log($this);
 
     if($this === ""){
       $result_blocks.addClass("show-result");
     }
     else{
       $result_blocks.each(function(){
-        var content = $(this).find("h2").text().toLowerCase();
+        var content = $(this).find(".searchable").text().toLowerCase();
         $(this).removeClass("show-result");
         if(content.indexOf($this) >= 0){
           $(this).addClass("show-result");
