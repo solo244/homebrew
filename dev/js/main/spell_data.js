8a18ad0e2c12288da3ba02c5605c45748f73a76f
Solo244 <solo244@gmail.com>
2017-08-01 15:38:34 +0200

    Added spell: Cloudkill

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -773,11 +773,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S",
+    class: "Sorcerer, Wizard",
+    duration: "Concentration, up to 10 minutes",
+    range: "120 feet",
+    description: "You create a 20-foot-radius sphere of poisonous, yellow-green fog centered on a point you choose within range. The fog spreads around corners. It lasts for the duration or until strong wind dispereses the fog, ending the spell. Its area is heavily obscured. When a creature enters the spell’s area for the first time on a turn or starts its turn there, that creature must make a Constitution saving throw. The creature takes 5d8 poison damageon a failed save, or half as much damage on a successful one. Creatures are affected even if they hold their breath or don’t need to breathe. The fog moves 10 feet away from you at the start of each of your turns, rolling along the surface of the ground. The vapors, being heavier than air, sink to the lowest level of the land, even pouring down openings. When you cast this spell using a spell slot of 6th level or higher, the damage increases by 1d8 for each slot level above 5th."
   },
   {
     name: "Color Spray",
