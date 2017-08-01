5de0c4d6260777ea0b8b38e189bf4bae364c7d90
Solo244 <solo244@gmail.com>
2017-08-01 16:19:12 +0200

    Added spell: Contingency

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1033,11 +1033,11 @@ var spell_data = [
     casting_time: "10 Minutes",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Wizard",
+    duration: "10 Days",
+    range: "Self",
+    description: "(a statuette of yourself carved from ivory and decorated with gems worth at least 1,500 gp) - Choose a spell of 5th level or lower that you can cast, that has a casting time of 1 action, and that can target you. You cast that spell called the contingent spell as part of casting contingency, expending spell slots for both, but the contingent spell doesn’t come into effect. Instead, it takes effect when a certain circumstance occurs. You describe that circumstance when you cast the two spells. For example, a contingency cast with water breathing might stipulate that water breathing comes into effect when you are engulfed in water or a similar liquid. The contingent spell takes effect immediately after the circumstance is met for the first time, whether or not you want it to. and then contingency ends. The contingent spell takes effect only on you, even if it can normally target others. You can use only one contingency spell at a time. If you cast this spell again, the effect of another contingency spell on you ends. Also, contingency ends on you if its material component is ever not on your person."
   },
   {
     name: "Continual Flame",
