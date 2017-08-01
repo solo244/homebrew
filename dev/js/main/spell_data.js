09c4f3c49d57f7d22b4098438909daa97b028789
Solo244 <solo244@gmail.com>
2017-08-01 15:20:17 +0200

    Added spell: Chromatic Orb

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -695,11 +695,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S, M",
+    class: "Sorcerer, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "90 feet",
+    description: "(a diamond worth at least 50 gp) - You hurl a 4-inch-diameter sphere of energy at a creature that you can see within range. You choose acid, cold, fire, lightning, poison, or thunder for the type of orb you create, and then make a ranged spell attack against the target. If the attack hits, the creature takes 3d8 damage of the type you chose. When you cast this spell using a spell slot of 2nd level or higher, the damage increases by 1d8 for each slot level above 1st."
   },
   {
     name: "Circle of Death",
