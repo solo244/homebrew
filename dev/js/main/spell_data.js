f926b406c36cdbbb9e20affc40963544855a5b8d
Solo244 <solo244@gmail.com>
2017-08-01 23:01:19 +0200

    Added spell: Cordon of Arrows

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1111,11 +1111,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "S",
+    class: "Sorcerer, Warlock, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "60 feet",
+    description: "1 reaction, which you take when you see a creature within 60 feet of you casting a spell. You attempt to interrupt a creature in the process of casting a spell. If the creature is casting a spell of 3rd level or lower, its spell fails and has no effect. If it is casting a spell of 4th level or higher, make an ability check using your spellcasting ability. The DC equals 10+ the spell’s level. On a success, the creature’s spell fails and has no effect. When you cast this spell using a spell slot of 4th level or higher, the interrupted spell has no effect if its level is less than or equal to the level of the spell slot you used."
   },
   {
     name: "Counterspell",
