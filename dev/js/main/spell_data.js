2d7a0a1847a90310bd68ed35cd38aad18cbab7c9
Solo244 <solo244@gmail.com>
2017-08-01 15:25:16 +0200

    Added spell: Clairvoyance

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -734,11 +734,11 @@ var spell_data = [
     casting_time: "10 Minutes",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: " V, S, M",
+    class: "Bard, Cleric, Sorcerer, Wizard",
+    duration: "Concentration, up to 10 minutes",
+    range: "1 mile",
+    description: "(a focus worth at least 100 gp, either a jeweled horn for hearing or a glass eye for seeing) - You create an invisible sensor within range in a location familiar to you (a place you have visited or seen before) or in an obvious location that is unfamiliar to you (such as behind a door, around a corner, or in a grove of trees). The sensor remains in place for the duration, and it can’t be attacked or otherwise interacted with. When you cast the spell, you choose seeing or hearing. You can use the chosen sense through the sensor as if you were in its space. As your action, you can switch between seeing and hearing. A creature that can see the sensor (such as a creature benefitting from see invisibility or truesight) sees a luminous, intangible orb about the size of your fist."
   },
   {
     name: "Clone",
