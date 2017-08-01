be229c7ff4ffff233eab616f3d56383e10ad6978
Solo244 <solo244@gmail.com>
2017-08-01 15:49:21 +0200

    Added spell: Command

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -799,11 +799,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V",
+    class: "Cleric, Paladin",
+    duration: "1 round",
+    range: "60 feet",
+    description: "You speak a one-word command to a creature you can see within range. The target must succeed on a Wisdom saving throw or follow the command on its next turn. The spell has no effect if the target is undead, if it doesn’t understand your language, or if your command is directly harmful to it. Some typical commands and their effects follow. You might issue a command other than one described here. If you do so, the DM determines how the target behaves. If the target can’t follow your command, the spell ends: Approach, Drop, Flee, Grovel, Halt. When you cast this spell using a spell slot of 2nd level or higher, you can affect one additional creature for each slot level above 1st. The creatures must be within 30 feet of each other when you target them"
   },
   {
     name: "Commune (Ritual)",
