4820e86fe96cf836a4f17028f59b61826d0c32d5
Solo244 <solo244@gmail.com>
2017-08-01 22:48:48 +0200

    Added spell: Control Water

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1072,11 +1072,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V,S,M",
+    class: "Cleric, Druid, Wizard",
+    duration: "Concentration, up to 10 minutes",
+    range: "300 feet",
+    description: "(a drop of water and a pinch of dust) - Until the spell ends, you control any freestanding water inside an area you choose that is a cube up to 100 feet on a side. You can choose from any of the following effects when you cast this spell. As an action on your turn, you can repeat the same effect or choose a different one: Flood, Part water, Redirect Flow, Whirlpool"
   },
   {
     name: "Control Weather",
