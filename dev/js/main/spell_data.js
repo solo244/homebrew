b935770dee8826ea5e5aed668ffb5cd842aa10ae
Solo244 <solo244@gmail.com>
2017-08-01 23:07:48 +0200

    Added spell: Create Bonfire

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1137,11 +1137,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S",
+    class: "Druid, Sorcerer, Warlock, Wizard",
+    duration: "Concentration, up to 1 minute",
+    range: "60 feet",
+    description: "You create a bonfire on ground that you can see within range. Until the spells ends, the bonfire fills a 5-foot cube. Any creature in the bonfire’s space when you cast the spell must succeed on a Dexterity saving throw or take 1d8 fire damage. A creature must also make the saving throw when it enters the bonfire’s space for the first time on a turn or ends its turn there. The spell’s damage increases by 1d8 when you reach 5th level (2d8), 11th level (3d8), and 17th level (4d8)."
   },
   {
     name: "Create Food and Water",
