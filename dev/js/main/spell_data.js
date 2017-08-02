aa63df91c5fdb97aa782f805d81b1ed40c1585a1
Solo244 <solo244@gmail.com>
2017-08-02 23:17:01 +0200

    Added spell: Fear

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1852,11 +1852,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Bard, Sorcerer, Warlock, Wizard",
+    duration: "Concentration, up to 1 minute",
+    range: "Self (30-foot cone)",
+    description: "(a white feather or the heart of a hen) - You project a phantasmal image of a creature’s worst fears. Each creature in a 30-foot cone must succeed on a Wisdom saving throw or drop whatever it is holding and become frightened for the duration. While frightened by this spell, a creature must take the Dash action and move away from you by the safest available route on each of its turns, unless there is nowhere to move. If the creature ends its turn in a location where it doesn’t have line of sight to you, the creature can make a Wisdom saving throw. On a successful save, the spell ends for that creature."
   },
   {
     name: "Feather Fall",
