6f5292166a5c932df0c7514f6f7f07ecab6173e6
Solo244 <solo244@gmail.com>
2017-08-02 23:28:16 +0200

    Added spell: Fire Bolt

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1969,11 +1969,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S",
+    class: "Sorcerer, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "120 feet",
+    description: "You hurl a mote of fire at a creature or object within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 fire damage. A flammable object hit by this spell ignites if it isn’t being worn or carried. This spell’s damage increases by 1d10 when you reach 5th level (2d10), 11th level (3d10), and 17th level (4d10)."
   },
   {
     name: "Fire Shield",
