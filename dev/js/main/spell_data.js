d8bf0bcf5457f71e8123d28bd3fece511f22048f
Solo244 <solo244@gmail.com>
2017-08-02 07:42:12 +0200

    Added spell: Erupting Earth

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1748,11 +1748,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S, M",
+    class: "Druid, Sorcerer, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "120 feet",
+    description: "(a piece of obsidian) - Choose a point you can see on the ground within range. A fountain of churned earth and stone erupts in a 20-foot cube centered on that point. Each creature in that area must make a Dexterity saving throw. A creature takes 3d12 bludgeoning damage on a failed save, or half as much damage on a successful one. Additionally, the ground in that area becomes difficult terrain until cleared away. Each 5-foot-square portion of the area requires at least 1 minute to clear by hand. When you cast this spell using a spell slot of 3rd level or higher, the damage increases by 1d12 for each slot level above 2nd."
   },
   {
     name: "Etherealness",
