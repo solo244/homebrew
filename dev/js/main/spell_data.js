d3603ceb152b6b7c4841b942f9f8865b6468553e
Solo244 <solo244@gmail.com>
2017-08-02 23:32:30 +0200

    Added spell: Fire Shield

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1982,11 +1982,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Wizard",
+    duration: "10 Minutes",
+    range: "Self",
+    description: "(a bit of phosphorus or a firefly) - Thin and wispy flames wreathe your body for the duration, shedding bright light in a 10-foot radius and dim light for an additional 10 feet, You can end the spell early by using an action to dismiss it.The flames provide you with a warm shield or a chill shield, as you choose. The warm shield grants you resistance to cold damage, and the chill shield grants you resistance to fire damage. In addition, whenever a creature within 5 feet of you hits you with a melee attack, the shield erupts with flame. The attacker takes 2d8 fire damage from a warm shield, or 2d8 cold damage from a cold shield."
   },
   {
     name: "Fire Storm",
