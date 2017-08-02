3dffbf35be09c16fa089ff20707eccefcdda683a
Solo244 <solo244@gmail.com>
2017-08-02 07:32:10 +0200

    Added spell: Drawmij’s Instant Summons

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1553,11 +1553,11 @@ var spell_data = [
     casting_time: "1 Minute",
     ritual: "Yes",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Wizard",
+    duration: "Until dispelled",
+    range: "Touch",
+    description: "(a sapphire worth 1,000 gp) - You touch an object weighing 10 pounds or less whose longest dimension is 6 feet or less. The spell leaves an invisible mark on its surface and invisibly inscribes the name of the item on the sapphire you use as the material component. Each time you cast this spell, you must use a different sapphire. At any time thereafter, you can use your action to speak the item’s name and crush the sapphire. The item instantly appears in your hand regardless of physical or planar distances, and the spell ends. If another creature is holding or carrying the item, crushing the sapphire doesn’t transport the item to you, but instead you learn who the creature possessing the object is and roughly where that creature is located at that moment. Dispel magic or a similar effect successfully applied to the sapphire ends this spell’s effect."
   },
   {
     name: "Dream",
