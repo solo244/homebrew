988c01603b32a629d35357d2501a3e2c83966855
Solo244 <solo244@gmail.com>
2017-08-02 23:42:17 +0200

    Added spell: Fly

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2086,11 +2086,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Sorcerer, Warlock, Wizard",
+    duration: "Concentration, up to 10 minutes",
+    range: "Touch",
+    description: "(a wing feather from any bird) - You touch a willing creature. The target gains a flying speed of 60 feet for the duration. When the spell ends, the target falls if it is still aloft, unless it can stop the fall. When you cast this spell using a spell slot of 4th level or higher, you can target one additional creature for each slot level above 3rd."
   },
   {
     name: "Fog Cloud",
