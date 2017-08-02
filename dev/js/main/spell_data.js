24670910120f1fddc470f4cd90af6ad7f7f10671
Solo244 <solo244@gmail.com>
2017-08-02 23:24:40 +0200

    Added spell: Feign Death

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1891,11 +1891,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "Yes",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S, M",
+    class: "Bard, Cleric, Druid, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "Touch",
+    description: "(a pinch of graveyard dirt) - You touch a willing creature and put it into a cataleptic state that is indistinguishable from death. For the spell’s duration, or until you use an action to touch the target and dismiss the spell, the target appears dead to all outward inspection and to spells used to determine the target’s status. The target is blinded and incapacitated, and its speed drops to 0. The target has resistance to all damage except psychic damage. If the target is diseased or poisoned when you cast the spell, or becomes diseased or poisoned while under the spell’s effect, the disease and poison have no effect until the spell ends."
   },
   {
     name: "Find Familiar (Ritual)",
