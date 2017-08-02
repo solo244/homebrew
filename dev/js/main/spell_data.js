36e47097c3e7b6d33289cc9cf30da7a18c07fb53
Solo244 <solo244@gmail.com>
2017-08-02 07:39:08 +0200

    Added spell: Elemental Bane

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1657,11 +1657,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S",
+    class: "Druid, Wizard, Warlock",
+    duration: "Concentration, up to 1 minute",
+    range: "90 feet",
+    description: "Choose one creature you can see within range, and choose one of the following damage types: acid, cold, fire, lightning, or thunder. The target must succeed on a Constitution saving throw or be affected by the spell for its duration. The first time each turn the affected target takes damage of the chosen type, the target takes an extra 2d6 damage of that type. Moreover, the target loses any resistance to that damage type until the spell ends. When you cast this spell using a spell slot of 5th level or higher, you can target one additional creature for each slot level above 4th. The creatures must be within 30 feet of each other when you target them."
   },
   {
     name: "Elemental Weapon",
