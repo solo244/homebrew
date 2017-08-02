05e13e0c9be6369b4abcd7201f82caf6b3ef865a
Solo244 <solo244@gmail.com>
2017-08-02 07:20:51 +0200

    Added spell: Demiplane

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1319,11 +1319,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "S",
+    class: "Warlock, Wizard",
+    duration: "1 Hour",
+    range: "60 feet",
+    description: "You create a shadowy door on a flat solid surface that you can see within range. The door is large enough to allow Medium creatures to pass through unhindered. When opened, the door leads to a demiplane that appears to be an empty room 30 feet in each dimension, made of wood or stone. When the spell ends, the door disappears, and any creatures or objects inside the demiplane remain trapped there, as the door also disappears from the other side. Each time you cast this spell, you can create a new demiplane, or have the shadowy door connect to a demiplane you created with a previous casting of this spell. Additionally, if you know the nature and contents of a demiplane created by a casting of this spell by another creature, you can have the shadowy door connect to its demiplane instead."
   },
   {
     name: "Destructive Wave",
