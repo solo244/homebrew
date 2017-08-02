38db0c71bf314c088680591523f74b86da297105
Solo244 <solo244@gmail.com>
2017-08-02 07:27:55 +0200

    Added spell: Dispel Magic

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1449,11 +1449,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S",
+    class: "Bard, Cleric, Druid, Paladin, Sorcerer, Warlock, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "120 feet",
+    description: "Choose any creature, object, or magical effect within range. Any spell of 3rd level or lower on the target ends. For each spell of 4th level or higher on the target, make an ability check using your spellcasting ability. The DC equals 10 + the spell’s level. On a successful check, the spell ends. When you cast this spell using a spell slot of 4th level or higher, you automatically end the effects of a spell on the target if the spell’s level is equal to or less than the level of the spell slot you used."
   },
   {
     name: "Dissonant Whispers",
