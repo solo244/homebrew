474a4a3fb4f0984b2213c6eac9f7ceef2e8075f0
Solo244 <solo244@gmail.com>
2017-08-03 22:54:53 +0200

    Added spell: Investiture of Flame

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2801,11 +2801,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S",
+    class: "Druid, Sorcerer, Warlock, Wizard",
+    duration: "Concentration, up to 10 minutes",
+    range: "Self",
+    description: "Flames race across your body, shedding bright light in a 30-foot radius and dim light for an additional 30 feet for the spell’s duration. The flames don’t harm you. Until the spell ends, you gain the following benefits: You are immune to fire damage and have resistance to cold damage, Any creature that moves within 5 feet of you for the first time on a turn or ends its turn there takes 1d10 fire damage, You can use your action to create a line of fire 15 feet long and 5 feet wide extending from you in a direction you choose. Each creature in the line must make a Dexterity saving throw. A creature takes 4d8 fire damage on a failed save, or half as much damage on a successful one."
   },
   {
     name: "Investiture of Ice",
