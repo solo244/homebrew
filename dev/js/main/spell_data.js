73417c562eb5fbfef6188930544f82cef84f725b
Solo244 <solo244@gmail.com>
2017-08-03 22:56:59 +0200

    Added spell: Investiture of Ice

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2814,11 +2814,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S",
+    class: "Druid, Sorcerer, Warlock, Wizard",
+    duration: "Concentration, up to 10 minutes",
+    range: "Self",
+    description: "Until the spell ends, ice rimes your body, and you gain the following benefits: You are immune to cold damage and have resistance to fire damage, You can move across difficult terrain created by ice or snow without spending extra movement, The ground in a 10-foot radius around you is icy and is difficult terrain for creatures other than you. The radius moves with you, You can use your action to create a 15-foot cone of freezing wind extending from your outstretched hand in a direction you choose. Each creature in the cone must make a Constitution saving throw. A creature takes 4d6 cold damage on a failed save, or half as much damage on a successful one. A creature that fails its save against this effect has its speed halved until the start of your next turn."
   },
   {
     name: "Investiture of Stone",
