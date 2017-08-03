62dc136a9a16c1ec4e1cac987575c5f76738c9bd
Solo244 <solo244@gmail.com>
2017-08-03 22:45:56 +0200

    Added spell: Immolation

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2736,11 +2736,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V",
+    class: "Sorcerer, Wizard",
+    duration: "Concentration, up to 1 minute",
+    range: "90 feet",
+    description: "Flames wreathe one creature you can see within range. The target must make a Dexterity saving throw. It takes 7d6 fire damage on a failed save, or half as much damage on a successful one. On a failed save, the target also burns for the spell’s duration. The burning target sheds bright light in a 30-foot radius and dim light for an additional 30 feet. At the end of each of its turns, the target repeats the saving throw. It takes 3d6 fire damage on a failed save, and the spell ends on a successful one. These magical flames can’t be extinguished through nonmagical means. If damage from this spell reduces a target to 0 hit points, the target is turned to ash."
   },
   {
     name: "Imprisonment",
