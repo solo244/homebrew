75fc7d8f4b38072769df3c92df924545b115379c
Solo244 <solo244@gmail.com>
2017-08-03 12:27:36 +0200

    Added spell: Gentle Repose

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2229,11 +2229,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "Yes",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Cleric, Wizard",
+    duration: "10 Days",
+    range: "Touch",
+    description: "(a pinch of salt and one copper piece placed on each of the corpse’s eyes, which must remain there for the duration) - You touch a corpse or other remains. For the duration, the target is protected from decay and can’t become undead. The spell also effectively extends the time limit on raising the target from the dead, since days spent under the influence of this spell don’t count against the time limit of spells such as raise dead."
   },
   {
     name: "Giant Insect",
