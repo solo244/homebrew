43a5dbb8be96fa035540818ba59f8d79e8d2260b
Solo244 <solo244@gmail.com>
2017-08-03 12:38:24 +0200

    Added spell: Gust of Wind

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2437,11 +2437,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Druid, Sorcerer, Wizard",
+    duration: "Concentration, up to 1 minute",
+    range: "Self (60-foot line)",
+    description: "(a legume seed) - A line of strong wind 60 feet long and 10 feet wide blasts from you in a direction you choose for the spell’s duration. Each creature that starts its turn in the line must succeed on a Strength saving throw or be pushed 15 feet away from you in a direction following the line. Any creature in the line must spend 2 feet of movement for every 1 foot it moves when moving closer to you. The gust disperses gas or vapor, and it extinguishes candles, torches, and similar unprotected flames in the area. It causes protected flames, such as those of lanterns, to dance wildly and has a 50 percent chance to extinguish them. As a bonus action on each of your turns before the spell ends, you can change the direction in which the line blasts from you."
   },
   {
     name: "Hail of Thorns",
