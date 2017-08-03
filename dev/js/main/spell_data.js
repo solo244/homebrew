6064d9d6ecee5ff1cd3670f52b7f3e61298869b0
Solo244 <solo244@gmail.com>
2017-08-03 22:40:15 +0200

    Added spell: Ice Knife

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2684,11 +2684,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "S, M",
+    class: "Druid, Sorcerer, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "60 feet",
+    description: "(a drop of water or piece of ice) - You create a shard of ice and fling it at one creature within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 piercing damage. Hit or miss, the shard then explodes. The target and each creature within 5 feet of the point where the ice exploded must succeed on a Dexterity saving throw or take 2d6 cold damage. At Higher Levels. When you cast this spell using a spell slot of 2nd level or higher, the cold damage increases by 1d6 for each slot level above 1st."
   },
   {
     name: "Ice Storm",
