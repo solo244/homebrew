ff86c871e23fd016e5bbe50d9478897aa5496684
Solo244 <solo244@gmail.com>
2017-08-03 22:42:52 +0200

    Added spell: Identify

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2710,11 +2710,11 @@ var spell_data = [
     casting_time: "1 Minute",
     ritual: "Yes",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S, M",
+    class: "Bard, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "Touch",
+    description: "(a pearl worth at least 100 gp and an owl feather) - You choose one object that you must touch throughout the casting of the spell. If it is a magic item or some other magic-imbued object, you learn its properties and how to use them, whether it requires attunement to use, and how many charges it has, if any. You learn whether any spells are affecting the item and what they are. If the item was created by a spell, you learn which spell created it. If you instead touch a creature throughout the casting, you learn what spells, if any, are currently affecting it."
   },
   {
     name: "Illusory Script (Ritual)",
