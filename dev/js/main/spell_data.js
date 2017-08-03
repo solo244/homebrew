e66c2dd6d6a457047bc37cadac3d3950f07ed1c6
Solo244 <solo244@gmail.com>
2017-08-03 19:28:18 +0200

    Added spell: Haste

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2502,11 +2502,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Sorcerer, Wizard",
+    duration: "Concentration, up to 1 minute",
+    range: "30 feet",
+    description: "(a shaving of licorice root) - Choose a willing creature that you can see within range. Until the spell ends, the target’s speed is doubled, it gains a +2 bonus to AC, it has advantage on Dexterity saving throws, and it gains an additional action on each of its turns. That action can be used only to take the Attack (one weapon attack only), Dash, Disengage, Hide, or Use an Object action. When the spell ends, the target can’t move or take actions until after its next turn, as a wave of lethargy sweeps over it."
   },
   {
     name: "Heal",
