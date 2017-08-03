9ae40919b7b1640b8f0075335b451baf92ca33a7
Solo244 <solo244@gmail.com>
2017-08-03 19:39:19 +0200

    Added spell: Hypnotic Pattern

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2671,11 +2671,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "S, M",
+    class: "Bard, Sorcerer, Warlock, Wizard",
+    duration: "Concentration, up to 1 minute",
+    range: "120 feet",
+    description: "(a glowing stick of incense or a crystal vial filled with phosphorescent material) - You create a twisting pattern of colors that weaves through the air inside a 30-foot cube within range. The pattern appears for a moment and vanishes. Each creature in the area who sees the pattern must make a Wisdom saving throw. On a failed save, the creature becomes charmed for the duration. While charmed by this spell, the creature is incapacitated and has a speed of 0. The spell ends for an affected creature if it takes any damage or if someone else uses an action to shake the creature out of its stupor."
   },
   {
     name: "Ice Knife",
