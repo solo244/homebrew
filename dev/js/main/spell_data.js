27c1129bc5d0d5dd9efa93ddc34107cb34cbdea0
Solo244 <solo244@gmail.com>
2017-08-03 19:31:30 +0200

    Added spell: Hex

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2593,11 +2593,11 @@ var spell_data = [
     casting_time: "1 Bonus",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
+    components: "V, S, M",
+    class: "Warlock",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "90 feet",
+    description: "(the petrified eye of a newt) - You place a curse on a creature that you can see within range. Until the spell ends, you deal an extra 1d6 necrotic damage to the target whenever you hit it with an attack. Also, choose one ability when you cast the spell. The target has disadvantage on ability checks made with the chosen ability. If the target drops to 0 hit points before this spell ends, you can use a bonus action on a subsequent turn of yours to curse a new creature. A remove curse cast on the target ends this spell early. When you cast this spell using a spell slot of 3rd or 4th level, you can maintain your concentration on the spell for up to 8 hours. When you use a spell slot of 5th level or higher, you can maintain your concentration on the spell for up to 24 hours."
   },
   {
     name: "Hold Monster",
