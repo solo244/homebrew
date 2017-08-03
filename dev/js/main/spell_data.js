f758e6beb24b9b4573ed72396e365a85417d30e1
Solo244 <solo244@gmail.com>
2017-08-03 12:24:34 +0200

    Added spell: Gaseous Form

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2190,11 +2190,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Sorcerer, Warlock, Wizard",
+    duration: "Concentration, up to 1 hour",
+    range: "Touch",
+    description: "(a bit of gauze and a wisp of smoke) - You transform a willing creature you touch, along with everything it’s wearing and carrying, into a misty cloud for the duration. The spell ends if the creature drops to 0 hit points. An incorporeal creature isn’t affected. While in this form, the target’s only method of movement is a flying speed of 10 feet. The target can enter and occupy the space of another creature. The target has resistance to nonmagical damage, and it has advantage on Strength, Dexterity, and Constitution saving throws. The target can pass through small holes, narrow openings, and even mere cracks, though it treats liquids as though they were solid surfaces. The target can’t fall and remains hovering in the air even when stunned or otherwise incapacitated. While in the form of a misty cloud, the target can’t talk or manipulate objects, and any objects it was carrying or holding can’t be dropped, used, or otherwise interacted with. The target can’t attack or cast spells."
   },
   {
     name: "Gate",
