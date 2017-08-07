359d8d63d63acc9d1763ac057e1c435b2bc960e7
Solo244 <solo244@gmail.com>
2017-08-07 07:31:23 +0200

    Added spell: Locate Object

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3035,11 +3035,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Bard, Cleric, Druid, Paladin, Ranger, Wizard",
+    duration: "Concentration, up to 10 minutes",
+    range: "Self",
+    description: "(a forked twig) - Describe or name an object that is familiar to you. You sense the direction to the object’s location, as long as that object is within 1,000 feet of you. If the object is in motion, you know the direction of its movement. The spell can locate a specific object known to you, as long as you have seen it up close – within 30 feet – at least once. Alternatively, the spell can locate the nearest object of a particular kind, such as a certain kind of apparel, jewelry, furniture, tool, or weapon. This spell can’t locate an object if any thickness of lead, even a thin sheet, blocks a direct path between you and the object."
   },
   {
     name: "Longstrider",
