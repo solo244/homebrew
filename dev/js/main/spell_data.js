6c9bf2f52178aa5f624959215bfa62ec785588a4
Solo244 <solo244@gmail.com>
2017-08-07 07:30:23 +0200

    Added spell: Locate Creature

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3022,11 +3022,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Bard, Cleric, Druid, Paladin, Ranger, Wizard",
+    duration: "Concentration, up to 1 hour",
+    range: "Self",
+    description: "(a bit of fur from a bloodhound) - Describe or name a creature that is familiar to you. You sense the direction to the creature’s location, as long as that creature is within 1,000 feet of you. If the creature is moving, you know the direction of its movement. The spell can locate a specific creature known to you, or the nearest creature of a specific kind (such as a human or a unicorn), so long as you have seen such a creature up close – within 30 feet – at least once. If the creature you described or named is in a different form, such as being unfer the effects of a polymorph spell, this spell doesn’t locate the creature. This spell can’t locate a creature if running water at least 10 feet wide blocks a direct path between you and the creature."
   },
   {
     name: "Locate Object",
