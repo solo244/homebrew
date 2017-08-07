c9e4be90ad85c0e9fd539b85108f6a7dee6a9630
Solo244 <solo244@gmail.com>
2017-08-07 07:29:09 +0200

    Added spell: Lightning Bolt

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2996,11 +2996,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V",
+    class: "Sorcerer, Warlock, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "15 feet",
+    description: "You create a lash of lightning energy that strikes at one creature of your choice that you can see within range. The target must succeed on a Strength saving throw or be pulled up to 10 feet in a straight line toward you and then take 1d8 lightning damage if it is within 5 feet of you. This spell's damage increases by 1d8 when you reach 5th level (2d8), 11th level (3d8), and 17th level (4d8)."
   },
   {
     name: "Locate Animals or Plants (Ritual)",
