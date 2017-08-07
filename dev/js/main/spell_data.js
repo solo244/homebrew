5d26934d487ca2a04b4cb2f500789bd1923407c0
Solo244 <solo244@gmail.com>
2017-08-07 07:25:30 +0200

    Added spell: Leomund’s Tiny Hut

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2918,11 +2918,11 @@ var spell_data = [
     casting_time: "1 Minute",
     ritual: "Yes",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Bard, Wizard",
+    duration: "8 Hours",
+    range: "Self (10-foot-radius hemisphere)",
+    description: "(a small crystal bead) - A 10-foot-radius immobile dome of force springs into existence around and above you and remains stationary for the duration. The spell ends if you leave its area. Nine creatures of Medium size or smaller can fit inside the dome with you. The spell fails if its area includes a larger creature or more than nine creatures. Creatures and objects within the dome when you cast this spell can move through it freely. All other creatures and objects are barred from passing through it. Spells and other magical effects can’t extend through the dome or be cast through it. The atmosphere inside the space is comfortable and dry, regardless of the weather outside. Until the spell ends, you can command the interior to become dimly lit or dark. The dome is opaque from the outside, of any color you choose, but it is transparent from the inside."
   },
   {
     name: "Lesser Restoration",
