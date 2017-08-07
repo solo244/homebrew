058fbb21e60ec35e2c9b93359bb75be7dcf347f5
Solo244 <solo244@gmail.com>
2017-08-07 07:26:27 +0200

    Added spell: Levitate

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2944,11 +2944,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Sorcerer, Wizard",
+    duration: "Concentration, up to 10 minutes",
+    range: "60 feet",
+    description: "(either a small leather loop or a piece of golden wire bent into a cup shape with a long shank on one end) - One creature or object of your choice that you can see within range rises vertically, up to 20 feet, and remains suspended there for the duration. The spell can levitate a target that weighs up to 500 pounds. An unwilling creature that succeeds on a Constitution saving throw is unaffected. The target can move only by pushing or pulling against a fixed object or surface within reach (such as a wall or a ceiling), which allows it to move as if it were climbing. You can change the target’s altitude by up to 20 feet in either direction on your turn. If you are the target, you can move up or down as part of your move. Otherwise, you can use your action to move the target, which must remain within the spell’s range. When the spell ends, the target floats gently to the ground if it is still aloft."
   },
   {
     name: "Light",
