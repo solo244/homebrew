6e2180794718890dd9a86da394bf2920f6310c18
Solo244 <solo244@gmail.com>
2017-08-07 07:27:22 +0200

    Added spell: Light

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -2957,11 +2957,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, M",
+    class: "Bard, Cleric, Sorcerer, Wizard",
+    duration: "1 Hour",
+    range: "Touch",
+    description: "(a firefly or phosphorescent moss) - You touch one object that is no larger than 10 feet in any dimension. Until the spell ends, the object sheds bright light in a 20-foot radius and dim light for an additional 20 feet. The light can be colored as you like. Completely covering the object with something opaque blocks the light. The spell ends if you cast it again or dismiss it as an action. If you target an object held or worn by a hostile creature, that creature must succeed on a Dexterity saving throw to avoid the spell."
   },
   {
     name: "Lightning Arrow",
