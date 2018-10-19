3a08fe0b95bddbc4330c924de03bd69ac74e9b1a
Solo244 <solo244@gmail.com>
2018-10-19 07:50:04 +0200

    Added Conjure Animals spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -903,11 +903,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S",
+    class: "Druid, Ranger",
+    duration: "1 Hour",
+    range: "60 feet",
+    description: "You summon fey spirits that take the form of beasts and appear in unoccupied spaces that you can see within range. Choose one of the following options for what appears: - One beast of challenge rating 2 or lower - Two beasts of challenge rating 1 or lower - Four beasts of challenge rating 1/2 or lower - Eight beasts of challenge rating 1/4 or lower. Each beast is also considered fey, and it disappears when it drops to 0 hit points or when the spell ends. The summoned creatures are friendly to you and your companions. Roll initiative for the summoned creatures as a group, which has its own turns. They obey any verbal commands that you issue to them (no action required by you). If you don’t issue any commands to them, they defend themselves from hostile creatures, but otherwise take no actions. The DM has the creatures’ statistics. When you cast this spell using certain higher-level spell slots, you choose one of the summoning options above, and more creatures appear: - twice as many with a 5th-level slot - three times as many with a 7th-level slot - four times as many with a 9th-level slot."
   },
   {
     name: "Conjure Barrage",
