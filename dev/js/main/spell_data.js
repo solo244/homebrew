5f2e32dd095a0b512e25b27c5a1907e77b1a6689
Solo244 <solo244@gmail.com>
2018-10-19 07:53:51 +0200

    Added Conjure Volley spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -981,11 +981,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S, M",
+    class: "Ranger",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "150 feet",
+    description: "You fire a piece of nonmagical ammunition from a ranged weapon or throw a nonmagical weapon into the air and choose a point within range. Hundreds of duplicates of the ammunition or weapon fall in a volley from above and then disappear. Each creature in a 40-foot-radius. 20-foot-high cylinder centered on that point must make a Dexterity saving throw. A creature takes 8d8 damage on a failed save, or half as much damage on a successful one. The damage type is the same as that of the ammunition or weapon."
   },
   {
     name: "Conjure Woodland Beings",
