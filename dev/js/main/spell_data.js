61288a9661cc6a8a8dc9b69f13d0638bbc9088de
Solo244 <solo244@gmail.com>
2018-10-19 07:47:11 +0200

    Added Compulsion spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -864,11 +864,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S",
+    class: "Bard",
+    duration: "1 Minute",
+    range: "30 feet",
+    description: "Creatures of your choice that you can see within range and that can hear you must make a Wisdom saving throw. A target automatically succeeds on this saving throw if it can’t be charmed. On a failed save, a target is affected by this spell. Until the spell ends, you can use a bonus action on each of your turns to designate a direction that is horizontal to you. Each affected target must use as much of its movement as possible to move in that direction on its next turn. It can take its action before it moves. After moving in this way, it can make another Wisdom saving throw to try to end the effect. A target isn’t compelled to move into an obviously deadly hazard, such as a fire pit, but it will provoke opportunity attacks to move in the designated direction."
   },
   {
     name: "Cone of Cold",
