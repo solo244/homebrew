007803f48b09a9045dade078e6465ce5f76fd4a5
Solo244 <solo244@gmail.com>
2018-10-19 07:52:48 +0200

    Added Conjure Celestial spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -929,11 +929,11 @@ var spell_data = [
     casting_time: "1 Minute",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S",
+    class: "Cleric",
+    duration: "1 Hour",
+    range: "90 feet",
+    description: "You summon a celestial of challenge rating 4 or lower, which appears in an unoccupied space that you can see within range. The celestial disappears when it drops to 0 hit points or when the spell ends. The celestial is friendly to you and your companions for the duration. Roll initiative for the celestial, which has its own turns. It obeys any verbal commands that you issue to it (no action required by you), as long as they don’t violate its alignment. If you don’t issue any commands to the celestial, it defends itself from hostile creatures but otherwise takes no actions. The DM has the celestial’s statistics. When you cast this spell using a 9th-level spell slot, you summon a celestial of challenge rating 5 or lower."
   },
   {
     name: "Conjure Elemental",
