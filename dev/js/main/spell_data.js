8413936bfe623d333eaa4c27c452c60e83cb5aa7
Solo244 <solo244@gmail.com>
2018-10-19 07:51:37 +0200

    Added Conjure Barrage spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -916,11 +916,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S, M",
+    class: "Ranger",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "Self",
+    description: "You throw a nonmagical weapon or fire a piece of nonmagical ammunition into the air to create a cone of identical weapons that shoot forward and then disappear. Each creature in a 60-foot cone must succeed on a Dexterity saving throw. A creature takes 3d8 damage on a failed save, or half as much damage on a successful one. The damage type is the same as that of the weapon or ammunition used as a component."
   },
   {
     name: "Conjure Celestial",
