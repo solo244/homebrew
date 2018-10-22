d29d841883d03cc0047076cfbd0f3569c7ab3fd6
Solo244 <solo244@gmail.com>
2018-10-22 09:50:10 +0200

    Added Cure Wounds spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1228,11 +1228,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S",
+    class: "Bard, Cleric, Druid, Paladin, Ranger",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "Touch",
+    description: "A creature you touch regains a number of hit points equal to 1d8 + your spellcasting ability modifier. This spell has no effect on undead or constructs. When you cast this spell using a spell slot of 2nd level or higher, the healing increases by 1d8 for each slot level above 1st."
   },
   {
     name: "Dancing Lights",
