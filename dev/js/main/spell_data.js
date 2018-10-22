b6f97c89443bdfea2e43546911df81cbe88dff48
Solo244 <solo244@gmail.com>
2018-10-22 09:54:00 +0200

    Added Detect Evil and Good spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1345,11 +1345,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S",
+    class: "Cleric, Paladin",
+    duration: "10 Minutes",
+    range: "Self",
+    description: "For the duration, you know if there is an aberration, celestial, elemental, fey, fiend, or undead within 30 feet of you, as well as where the creature is located. Similarly, you know if there is a place or object within 30 feet of you that has been magically consecrated or desecrated. The spell can penetrate most barriers, but it is blocked by 1 foot of stone, 1 inch of common metal, a thin sheet of lead, or 3 feet of wood or dirt."
   },
   {
     name: "Detect Magic (Ritual)",
