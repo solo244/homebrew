418eb0c8f1a565b11ffc0a43d0be8fd6224c6201
Solo244 <solo244@gmail.com>
2018-10-23 07:06:06 +0200

    Added Divination spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1475,11 +1475,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "Yes",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S, M",
+    class: "Cleric",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "Self",
+    description: "Your magic and an offering put you in contact with a god or a god’s servants. You ask a single question concerning a specific goal, event, or activity to occur within 7 days. The DM offers a truthful reply. The reply might be a short phrase, a cryptic rhyme, or an omen. The spell doesn’t take into account any possible circumstances that might change the outcome, such as the casting of additional spells or the loss or gain of a companion. If you cast this spell two or more times before finishing your next long rest, there is a cumulative 25 percent chance for each casting after the first that you get a random reading. The DM makes this roll in secret."
   },
   {
     name: "Divine Favor",
