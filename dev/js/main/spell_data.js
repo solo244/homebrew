b8a48fe86bd610ed14483547d4d73a3aefef4174
Solo244 <solo244@gmail.com>
2018-10-23 07:05:09 +0200

    Added Dissonant Whispers spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -1462,11 +1462,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V",
+    class: "Bard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "60 feet",
+    description: "You whisper a discordant melody that only one creature of your choice within range can hear, wracking it with terrible pain. The target must make a Wisdom saving throw. On a failed save, it takes 3d6 psychic damage and must immediately use its reaction , if available, to move as far as its speed allows away from you. The creature doesn’t move into obviously dangerous ground, such as a fire or a pit. On a successful save, the target takes half as much damage and doesn’t have to move away. A deafened creature automatically succeeds on the save. When you cast this spell using a spell slot of 2nd level or higher, the damage increases by 1d6 for each slot level above 1st"
   },
   {
     name: "Divination (Ritual)",
