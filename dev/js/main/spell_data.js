b0aae84d30823ceeb2b0391f41d1e9276944a5ca
Solo244 <solo244@gmail.com>
2018-10-24 07:19:25 +0200

    Added Mage Hand spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3087,11 +3087,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S",
+    class: "Bard, Sorcerer, Warlock, Wizard",
+    duration: "1 Minute",
+    range: "30 feet",
+    description: "A spectral, floating hand appears at a point you choose within range. The hand lasts for the duration or until you dismiss it as an action. The hand vanishes if it is ever more than 30 feet away from you or if you cast this spell again. You can use your action to control the hand. You can use the hand to manipulate an object, open an unlocked door or container, stow or retrieve an item from an open container, or pour the contents out of a vial. You can move the hand up to 30 feet each time you use it. The hand can’t attack, activate magical items, or carry more than 10 pounds."
   },
   {
     name: "Magic Circle",
