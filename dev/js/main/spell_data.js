c192a3b2799f2eadb3431d4e9dcd0d43ce53714c
Solo244 <solo244@gmail.com>
2018-10-25 07:21:27 +0200

    Added Mass Healing Word spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3217,11 +3217,11 @@ var spell_data = [
     casting_time: "1 Bonus",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V",
+    class: "Cleric",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "60 feet",
+    description: "As you call out words of restoration, up to six creatures of your choice that you can see within range regain hit points equal to 1d4 + your spellcasting ability modifier. This spell has no effect on undead or constructs. When you cast this spell using a spell slot of 4th level or higher, the healing increases by 1d4 for each slot level above 3rd."
   },
   {
     name: "Mass Suggestion",
