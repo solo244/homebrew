0da78949adb39150fbbf1b20007c449edd3ac00b
Solo244 <solo244@gmail.com>
2018-10-25 07:20:31 +0200

    Added Mass Heal spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3204,11 +3204,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S",
+    class: "Cleric",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "60 feet",
+    description: "A flood of healing energy flows from you into injured creatures around you. You restore up to 700 hit points, divided as you choose among any number of creatures that you can see within range. Creatures healed by this spell are also cured of all diseases and any effect making them blinded or deafened. This spell has no effect on undead or constructs."
   },
   {
     name: "Mass Healing Word",
