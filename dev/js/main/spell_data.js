7a66304cf9fbf8d279e975dc4af07477e5680f0e
Solo244 <solo244@gmail.com>
2018-10-25 07:24:51 +0200

    Added Mass Polymorph spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3224,6 +3224,19 @@ var spell_data = [
     description: "As you call out words of restoration, up to six creatures of your choice that you can see within range regain hit points equal to 1d4 + your spellcasting ability modifier. This spell has no effect on undead or constructs. When you cast this spell using a spell slot of 4th level or higher, the healing increases by 1d4 for each slot level above 3rd."
   },
   {
+    name: "Mass Polymorph",
+    level: 9,
+    school: "Transmutation",
+    casting_time: "1 Action",
+    ritual: "-",
+    concentration: "Yes",
+    components: "V, S, M",
+    class: "Bard, Sorcerer, Wizard",
+    duration: "1 Hour",
+    range: "120 feet",
+    description: "You transform up to ten creatures of your choice that you can see within range. An unwilling target must succeed on a Wisdom saving throw to resist the transformation. An unwilling shapechanger automatically succeeds on the save. Each target assumes a beast form of your choice, and you can choose the same form or different ones for each target. The new form can be any beast you have seen whose challenge rating is equal to or less than the target’s (or half the target’s level, if the target doesn’t have a challenge rating). The target’s game statistics, including mental ability scores, are replaced by the statistics of the chosen beast, but the target retains its hit points, alignment, and personality. Each target gains a number of temporary hit points equal to the hit points of its new form. These temporary hit points can’t be replaced by temporary hit points from another source. A target reverts to its normal form when it has no more temporary hit points or it dies. If the spell ends before then, the creature loses all its temporary hit points and reverts to its normal form. The creature is limited in the actions it can perform by the nature of its new form. It can’t speak, cast spells, or do anything else that requires hands or speech. The target’s gear melds into the new form. The target can’t activate, use, wield, or otherwise benefit from any of its equipment."
+  },
+  {
     name: "Mass Suggestion",
     level: 6,
     school: "Enchantment",
