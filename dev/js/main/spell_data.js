15ffb3c564e9487532146f743d894fce4d5fb5d0
Solo244 <solo244@gmail.com>
2018-10-26 12:13:29 +0200

    Added Melf’s Acid Arrow spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3295,11 +3295,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S, M",
+    class: "Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "90 feet",
+    description: "A shimmering green arrow streaks toward a target within range and bursts in a spray of acid. Make a ranged spell attack against the target. On a hit, the target takes 4d4 acid damage immediately and 2d4 acid damage at the end of its next turn. On a miss, the arrow splashes the target with acid for half as much of the initial damage and no damage at the end of its next turn. When you cast this spell using a spell slot of 3rd level or higher, the damage (both initial and later) increases by 1d4 for each slot level above 2nd."
   },
   {
     name: "Melf’s Minute Meteors",
