3c1f98237092c57a946f1a860bdac75ed4e8205b
Solo244 <solo244@gmail.com>
2018-10-26 12:16:52 +0200

    Added Meteor Swarm spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3347,11 +3347,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S",
+    class: "Sorcerer, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "1 mile",
+    description: "Blazing orbs of fire plummet to the ground at four different points you can see within range. Each creature in a 40-foot-radius sphere centered on each point you choose must make a Dexterity saving throw. The sphere spreads around corners. A creature takes 20d6 fire damage and 20d6 bludgeoning damage on a failed save, or half as much damage on a sucessful one. A creature in the area of more than one fiery burst is affected only once. The spell damages objects in the area and ignites flammable objects that aren’t being worn or carried."
   },
   {
     name: "Mind Blank",
