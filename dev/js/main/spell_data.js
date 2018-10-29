393be6b776e5f1f6b50ea3c8bbdcde01cafaf1e4
Solo244 <solo244@gmail.com>
2018-10-29 07:31:17 +0100

    Added Moonbeam spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3464,11 +3464,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Druid",
+    duration: "1 Minute",
+    range: "120 feet",
+    description: "A silvery beam of pale light shines down in a 5-footradius, 40-foot-high cylinder centered on a point within range. Until the spell ends, dim light fills the cylinder. When a creature enters the spell’s area for the first time on a turn or starts its turn there, it is engulfed in ghostly flames that cause searing pain, and it must make a Constitution saving throw. It takes 2d10 radiant damage on a failed save, or half as much damage on a successful one. A shapechanger makes its saving throw with disadvantage. If it fails, it also instantly reverts to its original form and can’t assume a different form until it leaves the spell’s light. On each of your turns after you cast this spell, you can use an action to move the beam 60 feet in any direction. When you cast this spell using aspell slot of 3rd level or higher, the damage increases by 1d1O for each slot level above 2nd."
   },
   {
     name: "Mordenkainen’s Faithful Hound",
