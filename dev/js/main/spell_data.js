8c8f03c451154b6c9dafb4488a6ccd547444b150
Solo244 <solo244@gmail.com>
2018-10-29 07:34:27 +0100

    Added Mordenkainen’s Sword spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3516,11 +3516,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Bard, Wizard",
+    duration: "1 Minute",
+    range: "60 feet",
+    description: "You create a sword-shaped plane of force that hovers within range. It lasts for the duration. When the sword appears, you make a melee spell attack against a target of your choice within 5 feet of the sword. On a hit. the target takes 3d10 force damage. Until the spell ends, you can use a bonus action on each of your turns to move the sword up to 20 feet to a spot you can see and repeat this attack against the same target or a different one."
   },
   {
     name: "Move Earth",
