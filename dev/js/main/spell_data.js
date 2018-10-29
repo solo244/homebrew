3080f42de9d71e6f3cdd0511f0c6e88713f32242
Solo244 <solo244@gmail.com>
2018-10-29 07:31:59 +0100

    Added Mordenkainen’s Faithful Hound spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3477,11 +3477,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Wizard",
+    duration: "8 Hours",
+    range: "30 feet",
+    description: "You conjure a phantom watchdog in an unoccupied space that you can see within range, where it remains for the duration, until you dismiss it as an action, or until you move more than 100 feet away from it. The hound is invisible to all creatures except you and can’t be harmed. When a Small or larger creature comes within 30 feet of it without first speaking the password that you specify when you cast this spell, the hound starts barking loudly. The hound sees invisible creatures and can see into the Ethereal Plane. It ignores illusions. At the start of each of your turns, the hound attempts to bite one creature within 5 feet of it that is hostile to you. The hound’s attack bonus is equal to your spellcasting ability modifier + your proficiency bonus. On a hit, it deals 4d8 piercing damage."
   },
   {
     name: "Mordenkainen’s Magnificent Mansion",
