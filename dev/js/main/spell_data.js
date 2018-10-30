de71e90011cbda32e14c958210459a47507ce1ab
Solo244 <solo244@gmail.com>
2018-10-30 07:24:44 +0100

    Added Phantom Steed (Ritual) spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3659,11 +3659,11 @@ var spell_data = [
     casting_time: "1 Minute",
     ritual: "Yes",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S",
+    class: "Wizard",
+    duration: "1 Hour",
+    range: "30 feet",
+    description: "A Large quasi-real, horselike creature appears on the ground in an unoccupied space of your choice within range. You decide the creature’s appearance, but it is equipped with a saddle, bit, and bridle. Any of the equipment created by the spell vanishes in a puff of smoke if it is carried more than 10 feet away from the steed. For the duration, you or a creature you choose can ride the steed. The creature uses the statistics for a riding horse, except it has a speed of 100 feet and can travel 10 miles in an hour, or 13 miles at a fast pace. When the spell ends, the steed gradually fades, giving the rider 1 minute to dismount. The spell ends if you use an action to dismiss it or if the steed takes any damage."
   },
   {
     name: "Planar Ally",
