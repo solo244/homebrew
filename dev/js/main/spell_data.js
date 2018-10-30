67802a887fb010f9f50b959b609043193c1446f8
Solo244 <solo244@gmail.com>
2018-10-30 07:21:42 +0100

    Added Passwall spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3620,11 +3620,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Wizard",
+    duration: "1 Hour",
+    range: "30 feet",
+    description: "A passage appears at a point of your choice that you can see on a wooden, plaster, or stone surface (such as a wall, a ceiling, or a floor) within range, and lasts for the duration. You choose the opening’s dimensions: up to 5 feet wide, 8 feet tall, and 20 feet deep. The passage creates no instability in a structure surrounding it. When the opening disappears, any creatures or objects still in the passage created by the spell are safely ejected to an unoccupied space nearest to the surface on which you cast the spell."
   },
   {
     name: "Phantasmal Force",
