344a1b2b6df1ac2a721effda75c4ecd22bd02bfc
Solo244 <solo244@gmail.com>
2018-11-02 10:37:42 +0100

    Added Prayer of Healing spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3789,11 +3789,11 @@ var spell_data = [
     casting_time: "10 Minutes",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V",
+    class: "Cleric",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "30 feet",
+    description: "Up to six creatures of your choice that you can see within range each regain hit points equal to 2d8 + your spellcasting ability modifier. This spell has no effect on undead or constructs. When you cast this spell using a spell slot of 3rd level or higher, the healing increases by 1d8 for each slot level above 2nd."
   },
   {
     name: "Prestidigitation",
