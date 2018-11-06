1d93d320320dcafe10852bab33f51723ffcaed2c
Solo244 <solo244@gmail.com>
2018-11-06 12:36:30 +0100

    Added Ray of Frost spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -3997,11 +3997,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S",
+    class: "Sorcerer, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "60 feet",
+    description: "A frigid beam of blue-white light streaks toward a creature within range. Make a ranged spell attack against the target. On a hit, it takes 1d8 cold damage, and its speed is reduced by 10 feet until the start of your next turn. The spell’s damage increases by 1d8 when you reach 5th level (2d8), 11th level (3d8), and 17th level (4d8)."
   },
   {
     name: "Ray of Sickness",
