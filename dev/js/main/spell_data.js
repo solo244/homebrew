62522be6b4e0fcb64bd7a45d1181658df63a8739
Solo244 <solo244@gmail.com>
2018-11-06 12:46:41 +0100

    Added Rope Trick spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -4114,11 +4114,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Wizard",
+    duration: "1 Hour",
+    range: "Touch",
+    description: "You touch a length of rope that is up to 60 feet long. One end of the rope then rises into the air until the whole rope hangs perpendicular to the ground. At the upper end of the rope, an invisible entrance opens to an extradimensional space that lasts until the spell ends. The extradimensional space can be reached by climbing to the top of the rope. The space can hold as many as eight Medium or smaller creatures. The rope can be pulled into the space, making the rope disappear from view outside the space. Attacks and spells can’t cross through the entrance into or out of the extradimensional space, but those inside can see out of it as if through a 3-foot-by-5-foot window centered on the rope. Anything inside the extradimensional space drops out when the spell ends."
   },
   {
     name: "Sacred Flame",
