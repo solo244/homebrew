e64f4db3b3dcfb539c13e5da59d46b5d6ad4b25d
Solo244 <solo244@gmail.com>
2018-11-12 12:21:17 +0100

    Added Scrying spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -4166,11 +4166,11 @@ var spell_data = [
     casting_time: "10 Minutes",
     ritual: "-",
     concentration: "Yes",
-    components: "-",
-    class: "-",
-    duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    components: "V, S, M",
+    class: "Bard, Cleric, Druid, Warlock, Wizard",
+    duration: "10 Minutes",
+    range: "Self",
+    description: "You can see and hear a particular creature you choose that is on the same plane of existence as you. The target must make a W isdom saving throw, which is modified by how well you know the target and the sort of physical connection you have to it. If a target knows you’re casting this spell, it can fail the saving throw voluntarily if it wants to be observed. - Secondhand (you have heard of the target), Save Modifier +5 - Firsthand (you have met the target), Save Modifier +0 - Familiar (you know the target well), Save Modifier -5 -- Likeness or picture, Save Modifier -2 - Posession or garment, Save Modifier -4 - Body part, lock of hair, bit of nail, or the like, Save Modifier -10. -- On a successful save, the target isn’t affected, and you can’t use this spell against it again for 24 hours. On a failed save, the spell creates an invisible sensor within 10 feet of the target. You can see and hear through the sensor as if you w ere there. The sensor moves with the target, remaining within 10 feet of it for the duration. A creature that can see invisible objects sees the sensor as a luminous orb about the size of your fist. Instead of targeting a creature, you can choose a location you have seen before as the target of this spell. When you do, the sensor appears at that location and doesn’t move."
   },
   {
     name: "Searing Smite",
