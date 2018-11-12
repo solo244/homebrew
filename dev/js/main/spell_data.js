6328207c50b6c17397c2b4761e28a40013b447c2
Solo244 <solo244@gmail.com>
2018-11-12 12:16:16 +0100

    Added Sacred Flame spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -4127,11 +4127,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S",
+    class: "Cleric",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "60 feet",
+    description: "Flame-like radiance descends on a creature that you can see within range. The target must succeed on a Dexterity saving throw or take 1d8 radiant damage. The target gains no benefit from cover for this saving throw. The spell’s damage increases by 1d8 when you reach 5th level (2d8), 11th level (3d8), and 17th level (4d8)."
   },
   {
     name: "Sanctuary",
