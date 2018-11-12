58e241b842524acdc24ddc041924515a404a7473
Solo244 <solo244@gmail.com>
2018-11-12 12:18:10 +0100

    Added Scorching Ray spell

--- dev/js/main/spell_data.js
+++ dev/js/main/spell_data.js
@@ -4153,11 +4153,11 @@ var spell_data = [
     casting_time: "1 Action",
     ritual: "-",
     concentration: "-",
-    components: "-",
-    class: "-",
+    components: "V, S",
+    class: "Sorcerer, Wizard",
     duration: "Instantaneous",
-    range: "0 feet",
-    description: "Lorem ipsum"
+    range: "120 feet",
+    description: "You create three rays of fire and hurl them at targets within range. You can hurl them at one target or several. Make a ranged spell attack for each ray. On a hit, the target takes 2d6 fire damage. When you cast this spell using a spell slot of 3rd level or higher, you create one additional ray for each slot level above 2nd."
   },
   {
     name: "Scrying",
