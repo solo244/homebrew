75381593eb45de895d6609e663fa8a8c283c074c
Solo244 <solo244@gmail.com>
2019-01-21 08:11:43 +0100

    First steps to sorting spell data

--- dev/js/main/spell_sort.js
+++ dev/js/main/spell_sort.js
@@ -1,178 +1,43 @@
 (function ($) {
 
-  /*
-   * Vars
-   */
   var $body = $("html, body"),
-      $sorting = $body.find(".sorting"),
-      $sort_c0 = $sorting.find(".c0"),
-      $sort_c1 = $sorting.find(".c1"),
-      $sort_c2 = $sorting.find(".c2"),
-      $sort_c3 = $sorting.find(".c3"),
-      $sort_c4 = $sorting.find(".c4"),
-      $sort_c5 = $sorting.find(".c5"),
-      $sort_c6 = $sorting.find(".c6");
+    $sorting = $body.find(".sorting"),
+    $filterCheckboxes = $sorting.find("input[type=checkbox]");
 
-  $sorting.find("input").on("click", reSort);
   setSpellCount();
 
-  /*
-   *
-   */
-  function reSort(e){
-    var $spells = $body.find(".spells");
+  $filterCheckboxes.on('change', function () {
+    var selectedFilters = {};
 
-    var selected_c0 = [],
-        selected_c1 = [],
-        selected_c2 = [],
-        selected_c3 = [],
-        selected_c4 = [],
-        selected_c5 = [],
-        selected_c6 = [];
-
-    $sort_c0.find("input").each(function() {
-      var $this = $(this),
-          $text = $this.closest("li").text().trim();
-
-      if($this.is(":checked")) {
-        selected_c0.push($text);
+    $filterCheckboxes.filter(':checked').each(function () {
+      if (!selectedFilters.hasOwnProperty(this.name)) {
+        selectedFilters[this.name] = [];
       }
+      selectedFilters[this.name].push(this.value);
     });
 
-    $sort_c1.find("input").each(function() {
-      var $this = $(this),
-          $text = $this.closest("li").text().trim();
+    var $filteredResults = $(".spells .spell");
 
-      if($this.is(":checked")) {
-        if($text === "Cantrip") {
-          $text = "0";
-        } else {
-          $text = $text.replace("Level ", "");
-        }
-        selected_c1.push($text);
-      }
+    $.each(selectedFilters, function (name, filterValues) {
+      $filteredResults = $filteredResults.filter(function () {
+        var matched = false,
+          currentFilterValues = $(this).data("category").split(" ");
+
+        $.each(currentFilterValues, function (_, currentFilterValue) {
+          if ($.inArray(currentFilterValue, filterValues) != -1) {
+            matched = true;
+            return false;
+          }
+        });
+
+        return matched;
+      });
     });
 
-    $sort_c2.find("input").each(function() {
-      var $this = $(this),
-          $text = $this.closest("li").text().trim();
-
-      if($this.is(":checked")) {
-        selected_c2.push($text);
-      }
-    });
-
-    $sort_c3.find("input").each(function() {
-      var $this = $(this),
-          $text = $this.closest("li").text().trim();
-
-      if($this.is(":checked")) {
-        if($text === "No") {
-          $text = "-";
-        }
-        selected_c3.push($text);
-      }
-    });
-
-    $sort_c4.find("input").each(function() {
-      var $this = $(this),
-          $text = $this.closest("li").text().trim();
-
-      if($this.is(":checked")) {
-        selected_c4.push($text);
-      }
-    });
-
-    $sort_c5.find("input").each(function() {
-      var $this = $(this),
-          $text = $this.closest("li").text().trim();
-
-      if($this.is(":checked")) {
-        selected_c5.push($text);
-      }
-    });
-
-    $sort_c6.find("input").each(function() {
-      var $this = $(this),
-          $text = $this.closest("li").text().trim();
-
-      if($this.is(":checked")) {
-        if($text === "No") {
-          $text = "-";
-        }
-        selected_c6.push($text);
-      }
-    });
-
-    if(selected_c0.length === 0) {
-      selected_c0 = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];
-    }
-    if(selected_c1.length === 0) {
-      selected_c1 = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-    }
-    if(selected_c2.length === 0) {
-      selected_c2 = ["Abjuration", "Conjuration", "Divination", "Enchantment", "Evocation", "Illusion", "Necromancy", "Transmutation"];
-    }
-    if(selected_c3.length === 0) {
-      selected_c3 = ["Yes", "-"];
-    }
-    if(selected_c4.length === 0) {
-      selected_c4 = ["1 Action", "1 Bonus", "1 Minute", "10 Minutes", "1 Hour", "8 Hours", "Special"];
-    }
-    if(selected_c5.length === 0) {
-      selected_c5 = ["V", "S", "M", "V, S", "V, M", "S, M", "V, S, M", "-"];
-    }
-    if(selected_c6.length === 0) {
-      selected_c6 = ["Yes", "-"];
-    }
-
-    // Show/hide spells
-    $spells.find(".spell").each(function() {
-      var $this = $(this),
-          $class = "" + $this.find(".c1").text(),
-          $level = "" + $this.find(".c2").text(),
-          $school = "" + $this.find(".c3").text(),
-          $ritual = "" + $this.find(".c4").text(),
-          $time = "" + $this.find(".c5").text(),
-          $components = "" + $this.find(".c6").text(),
-          $concentration = "" + $this.find(".c7").text();
-
-      $this.addClass("hide-spell");
-
-      var spellsMatch0 = selected_c0.indexOf($class),
-          spellsMatch1 = selected_c1.indexOf($level),
-          spellsMatch2 = selected_c2.indexOf($school),
-          spellsMatch3 = selected_c3.indexOf($ritual),
-          spellsMatch4 = selected_c4.indexOf($time),
-          spellsMatch5 = selected_c5.indexOf($components),
-          spellsMatch6 = selected_c6.indexOf($concentration);
-
-      /*if(spellsMatch0 >= 0 && spellsMatch1 >= 0 && spellsMatch2 >= 0 && spellsMatch3 >= 0 && spellsMatch4 >= 0 && spellsMatch5 >= 0 && spellsMatch6 >= 0) {
-        $this.removeClass("hide-spell");
-      }*/
-
-      if(spellsMatch1 >= 0 && spellsMatch2 >= 0 && spellsMatch3 >= 0 && spellsMatch4 >= 0 && spellsMatch5 >= 0 && spellsMatch6 >= 0) {
-        $this.removeClass("hide-spell");
-      }
-    });
-
-    // Animate to top
-    var timer = setInterval(function(){ delayAnimation(); }, 100);
-
-    function delayAnimation() {
-      $('html, body').animate({
-          scrollTop: $sorting.offset().top
-      }, 800);
-      clearInterval(timer);
-    }
-
-    // Count total spells
+    $(".spells .spell").addClass("hide-spell").filter($filteredResults).removeClass("hide-spell");
     setSpellCount();
-  }
+  });
 
-  /*
-   *
-   */
   function setSpellCount() {
     var total = $(".spell:not(.hide-spell)").length;
     $(".sorting .total").text(total);
--- dev/js/main/spells.js
+++ dev/js/main/spells.js
@@ -4,17 +4,36 @@
    * Vars
    */
   var $body = $("html, body"),
-      $spells = $body.find(".spells"),
-      max = spell_data.length;
+    $spells = $body.find(".spells"),
+    max = spell_data.length;
 
   setSpells();
 
   /*
    *
    */
-  function setSpells(e){
-    for(var i = 0; i < max; i++) {
-      $spells.append('<div class="spell"><div class="c1">' + spell_data[i].name + '</div><div class="c2">' + spell_data[i].level + '</div><div class="c3">' + spell_data[i].school + '</div><div class="c4">' + spell_data[i].ritual + '</div><div class="c5">' + spell_data[i].casting_time + '</div><div class="c6">' + spell_data[i].components + '</div><div class="c7">' + spell_data[i].concentration + '</div><div class="classes"><strong>Class:</strong> ' + spell_data[i].class + '</div><div class="description">' + spell_data[i].description + '</div></div>');
+  function setSpells(e) {
+    for (var i = 0; i < max; i++) {
+      var level = String(spell_data[i].level).toLowerCase(),
+          school = String(spell_data[i].school).toLowerCase(),
+          classes_raw = String(spell_data[i].class).toLowerCase(),
+          classes = classes_raw.replace(/\,/g, '');
+          data = level + " " + school + " " + classes;
+
+      console.log('data:', data);
+
+      $spells.append('\
+      <div class="spell" data-category="' + data + '">\
+        <div class="c1">' + spell_data[i].name + '</div>\
+        <div class="c2">' + spell_data[i].level + '</div>\
+        <div class="c3">' + spell_data[i].school + '</div>\
+        <div class="c4">' + spell_data[i].ritual + '</div>\
+        <div class="c5">' + spell_data[i].casting_time + '</div>\
+        <div class="c6">' + spell_data[i].components + '</div>\
+        <div class="c7">' + spell_data[i].concentration + '</div>\
+        <div class="classes"><strong>Class:</strong> ' + spell_data[i].class + '</div>\
+        <div class="description">' + spell_data[i].description + '</div>\
+      </div>');
     }
 
     $(".spell").on("click", openClose);
