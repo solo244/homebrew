17dd988cc2c7cae46663c81c6a8fbd7ae49ae87e
Solo244 <solo244@gmail.com>
2019-03-12 07:04:47 +0100

    Continue updating spells

--- dev/js/main/_level_4.js
+++ dev/js/main/_level_4.js
@@ -24,5 +24,44 @@ var spells_4 = [
     duration: "Concentration, up to 10 minutes",
     range: "Self, 30 foot radius",
     description: "Life-preserving energy radiates from you in an aura with a 30-foot radius. Until the spll ends, the aura moves with you, centered on you. Each nonhostile creature in the aura (including you) has resistance to necrotic damage, and its hit point maximum can’t be reduced. In addition, a nonhostile, living creature regains 1 hit point when it starts its turn in the arua with 0 hit points."
+  },
+  {
+    name: "Aura of Purity",
+    level: 4,
+    school: "Abjuration",
+    casting_time: "1 Action",
+    ritual: "-",
+    concentration: "Yes",
+    components: "V",
+    class: "Paladin",
+    duration: "Concentration, up to 10 minutes",
+    range: "Self, 30 foot radius",
+    description: "Purifying energy radiates from you in an aura with a 30-foot radius. Until the spell ends, the aura moves with you, centered on you. Each nonhostile creature in the aura (including you) can’t become diseased, has resistance to poison damage, and has advantage on saving throws against effects that cause any of the following conditions: blnded, charmed, deafended, frightened, paralyzed, poisoned, and stunned."
+  },
+  {
+    name: "Banishment",
+    level: 4,
+    school: "Abjuration",
+    casting_time: "1 Action",
+    ritual: "-",
+    concentration: "Yes",
+    components: "V, S, M",
+    class: "Cleric, Paladin, Sorcerer, Warlock, Wizard",
+    duration: "Concentration, up to 1 minute",
+    range: "60 feet",
+    description: "(an item distasteful to the target) - You attempt to send one creature that you can see within range to another place of existence. The target must succeed on a Charisma saving throw or be banished. If the target is native to the plane of existence you’re on, you banish the target to a harmless demiplane. While there, the target is incapacitated. The target remains there until the spell ends, at which point the target reappears in the space it left or in the nearest unoccupied space if that space is occupied. If the target is native to a different plane of existence that the one you’re on, the target is banished with a faint popping noise, returning to its home plane. If the spell ends before 1 minute has passed, the target reappears in the space it left or in the nearest unoccupied space if that space is occupied. Otherwise, the target doesn’t return. When you cast this spell using a spell slot of 5th level or higher, you can target one additional creature for each slot level above 4th."
+  },
+  {
+    name: "Blight",
+    level: 4,
+    school: "Necromancy",
+    casting_time: "1 Action",
+    ritual: "-",
+    concentration: "-",
+    components: "V, S",
+    class: "Druid, Sorcerer, Warlock, Wizard",
+    duration: "Instantaneous",
+    range: "30 feet",
+    description: "Necromantic energy washes over a creature of your choice that you can see within range, draining moisture and vitality from it. The target must make a Constitution saving throw. The target takes 8d8 necrotic damage on a failed save, or half as much damage on a successful one. This spell has no effect on undead or constructs. If you target a plant creature or a magical plant, it makes the saving throw with disadvantage, and the spell deals maximum damage to it. If you target a nonmagical plant that isn’t a creature, such as a tree or shrub, it doesn’t make a saving throw; it simply withers and dies. When you cast this spell using a spell slot of 5th level or higher, the damage increases by 1d8 for each slot level above 4th."
   }
 ];
--- dev/js/main/_level_6.js
+++ dev/js/main/_level_6.js
@@ -11,5 +11,57 @@ var spells_6 = [
     duration: "Concentration, up to 10 minutes",
     range: "500 feet",
     description: "You create linked teleportation portals that remain open for the duration. Choose two points on the ground that you can see, one point within 10 feet of you and one point within 500 feet of you. A circular portal, 10 feet in diameter, opens over each point. If the portal would open in the space occupied by a creature, the spell fails, and the casting is lost. The portals are two-dimensional glowing rings filled with mist, hovering inches from the ground and perpendicular to it at the points you choose. A ring is visible only from one side (your choice), which is the side that functions as a portal. Any creature or object entering the portal exits from the other portal as if the two were adjacent to each other; passing through a portal from the nonportal side has no effect. The mist that fills each portal is opaque and blocks vision through it. On your turn, you can rotate the rings as a bonus action so that the active side faces in a different direction."
+  },
+  {
+    name: "Blade Barrier",
+    level: 6,
+    school: "Evocation",
+    casting_time: "1 Action",
+    ritual: "-",
+    concentration: "Yes",
+    components: "V, S",
+    class: "Cleric",
+    duration: "Concentration, up to 10 minute",
+    range: "90 feet",
+    description: "You create a vertical wall of whirling, razor-sharp blades made of magical energy. The wall appears within range and lasts for the duration. You can make a straight wall up to 100 feet long, 20 feet high, and 5 feet thick, or a ringed wall up to 60 feet in diameter, 20 feet high, and 5 feet thick. The wall provides three-quarters cover to creatures behind it, and its space is difficult terrain. When a creature enters the wall’s area for the first time on a turn or starts its turn there, the creature must make a Dexterity saving throw. On a failed save, the creature takes 6 d10 slashing damage. On a successful save, the creature takes half as much damage."
+  },
+  {
+    name: "Bones of the Earth",
+    level: 6,
+    school: "Transmutation",
+    casting_time: "1 Action",
+    ritual: "-",
+    concentration: "-",
+    components: "V, S",
+    class: "Druid",
+    duration: "Instantaneous",
+    range: "120 feet",
+    description: "You cause up to six pillars of stone to burst from places on the ground that you can see within range. Each pillar is a cylinder that has a diameter of 5 feet and a height of up to 30 feet. The ground where a pillar appears must be wide enough for its diameter, and you can target ground under a creature if that creature is Medium or smaller. Each pillar has AC 5 and 30 hit points. When reduced to 0 hit points, a pillar crumbles into rubble, which creates an area of difficult terrain with a 10-foot radius. The rubble lasts until cleared. If a pillar is created under a creature, that creature must succeed on a Dexterity saving throw or be lifted by the pillar. A creature can choose to fail the save. If a pillar is prevented from reaching its full height because of a ceiling or other obstacle, a creature on the pillar takes 6d6 bludgeoning damage and is restrained, pinched between the pillar and the obstacle. The restrained creature can use an action to make a Strength or Dexterity check (the creature’s choice) against the spell’s saving throw DC. On a success, the creature is no longer restrained and must either move off the pillar or fall off it. At Higher Levels. When you cast this spell using a spell slot of 7th level or higher, you can create two additional pillars for each slot level above 6th."
+  },
+  {
+    name: "Chain Lightning",
+    level: 6,
+    school: "Evocation",
+    casting_time: "1 Action",
+    ritual: "-",
+    concentration: "-",
+    components: "V, S, M",
+    class: "Sorcerer, Wizard",
+    duration: "Instantaneous",
+    range: "150 feet",
+    description: "(a bit of fur; a piece of amber, glass, or a crystal rod; and three silver pins) - You create a bolt of lightning that arcs toward a target of your choice that you can see within range. Three bolts then leap from that target to as many as three other targets, each of which must be within 30 feet of the first target. A target can be a creature or an object and can be targeted by only one of the bolts. A target must make a Dexterity saving throw. The target takes 10d8 lightning damage on a failed save, or half as much on a successful one. When you cast this spell using a spell slot of 7th level or higher, one additional bolt leaps from the first target to another target for each slot level above 6th."
+  },
+  {
+    name: "Circle of Death",
+    level: 6,
+    school: "Necromancy",
+    casting_time: "1 Action",
+    ritual: "-",
+    concentration: "-",
+    components: "V, S, M",
+    class: "Sorcerer, Warlock, Wizard",
+    duration: "Instantaneous",
+    range: "150 feet",
+    description: "(the powder of a crushed black pearl worth at least 500 gp) - A sphere of negative energy ripples out in a 60-foot-radius sphere from a point within range. Each creature in that area must make a Constitution saving throw. A target takes 8d6 necrotic damage on a failed save, or half as much damage on a successful one. When you cast this spell using a spell slot of 7th level or higher, the damage increases by 2d6 for each slot level above 6th."
   }
 ];
